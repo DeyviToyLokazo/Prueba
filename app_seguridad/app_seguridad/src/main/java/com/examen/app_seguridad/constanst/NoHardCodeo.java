e24be6add7da4c6508808f4a7904f43dd7acc89a
DeyviToyLokazo <144736582+DeyviToyLokazo@users.noreply.github.com>
2025-01-21 10:51:03 -0500

    app_seguridad

---
+++ app_seguridad/app_seguridad/src/main/java/com/examen/app_seguridad/constanst/NoHardCodeo.java
@@ -0,0 +1,10 @@
+package com.examen.app_seguridad.constanst;
+
+public class NoHardCodeo {
+    public static final String USUARIO_BASE = "/api/seguridad";
+    public static final String GET_ALL = "/findAll";
+    public static final String CREATE = "/create";
+    public static final String GET_BY_ID = "/findById/{id}";
+    public static final String UPDATE = "/update";
+    public static final String DELETE = "/delete/{id}";
+}
